c92f4b265d7cb783709652b1a5acb8efc73bfb49
Deng <d.vay@qq.com>
2018-10-24 21:13:26 +0800

    Add init.sh to use symbolic link

--- .profile
+++ .profile
@@ -1,7 +1,14 @@
-
 # added by Miniconda3 installer
 export PATH="/opt/bin:$PATH"
 export PATH="/opt/miniconda3/bin:$PATH"
+
+# set homebrew mirror to tuna
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
 
 . /opt/miniconda3/etc/profile.d/conda.sh
 
---
+++ init.sh
@@ -0,0 +1,13 @@
+dir_url=$(pwd)
+
+ln -s "$dir_url/.profile" ~/.profile
+ln -s "$dir_url/.gitconfig" ~/.gitconfig
+ln -s "$dir_url/.gitignore" ~/.gitignore
+ln -s "$dir_url/.ideavimrc" ~/.ideavimrc
+ln -s "$dir_url/.oh-my-zsh/custom/themes/kolod.zsh-theme" ~/.oh-my-zsh/custom/themes/
+ln -s "$dir_url/.pip" ~/
+ln -s "$dir_url/.tmux.conf" ~/.tmux.conf
+ln -s "$dir_url/.zshrc" ~/.zshrc
+ln -s "$dir_url/.vimrc" ~/.vimrc
+ln -s "$dir_url/.condarc" ~/.condarc
+
