b332e0107600111489620d86f59a49599573b7ed
Deng <d.vay@qq.com>
2020-02-20 18:55:40 +0800

    Update config for Linux

--- init.sh
+++ init.sh
@@ -1,15 +1,21 @@
+#!/bin/bash
+
 dir_url=$(pwd)
 
 [[ -e ~/.profile ]] && mv ~/.profile ~/.profile.old
 ln -s "$dir_url/.profile" ~/
-ln -s "$dir_url/.gitconfig" ~/
-ln -s "$dir_url/.ideavimrc" ~/
+
+[[ -e ~/.zshrc ]] && mv ~/.zshrc ~/.zshrc.old
+ln -s "$dir_url/.zshrc" ~/
 ln -s "$dir_url/.zsh-custom" ~/
-ln -s "$dir_url/.pip" ~/
+
 ln -s "$dir_url/.vim" ~/
+ln -s "$dir_url/.vimrc" ~/
+ln -s "$dir_url/.ideavimrc" ~/
+
+ln -s "$dir_url/.pip" ~/
+ln -s "$dir_url/.condarc" ~/
 ln -s "$dir_url/.jupyter" ~/
 ln -s "$dir_url/.tmux.conf" ~/
-ln -s "$dir_url/.zshrc" ~/
-ln -s "$dir_url/.vimrc" ~/
-ln -s "$dir_url/.condarc" ~/
+ln -s "$dir_url/.gitconfig" ~/
 
